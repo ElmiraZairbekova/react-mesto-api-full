f7b2af332d61d849eb111fd17233be76d98ef208
ElmiraZairbekova <loveni-shop@yandex.ru>
2022-11-20 14:09:02 +0300

    PR15

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,82 +1,41 @@
 const BASE_URL = 'https://api.mesto.education.nomoredomains.icu';
 
-// const request = async ({
-//   url,
-//   method = 'POST',
-//   token,
-//   data,
-// }) => {
-//   const response = await fetch(`${BASE_URL}${url}`, {
-//     method,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       ...!!token && { 'Authorization': `Bearer ${token}` },
-//     },
-//     ...!!data && { body: JSON.stringify(data) },
-//   });
-//   const json = await response.json();
-//   return response.ok ? json : Promise.reject(json.message);
-// }
-
-// export function registerUser (email, password) {
-//   return request({
-//     url: '/signup',
-//     data: { email, password },
-//   })
-// };
-
-// export function loginUser (email, password) {
-//   return request({
-//     url: '/signin',
-//     data: { email, password },
-//   })
-// };
-
-// export function getToken (token) {
-//   return request({
-//     url: '/users/me',
-//     method: 'GET',
-//     token,
-//   })
-// }
-
-
-export function registerUser(email, password) {
-  return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
+const request = async ({
+  url,
+  method = 'POST',
+  token,
+  data,
+}) => {
+  const response = await fetch(`${BASE_URL}${url}`, {
+    method,
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json',
+      ...!!token && { 'Authorization': `Bearer ${token}` },
     },
-    body: JSON.stringify({ email, password }),
-  }).then(checkRes);
+    ...!!data && { body: JSON.stringify(data) },
+  });
+  const json = await response.json();
+  return response.ok ? json : Promise.reject(json.message);
 }
 
-export function loginUser(email, password) {
-  return fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  }).then(checkRes);
-}
+export function registerUser (email, password) {
+  return request({
+    url: '/signup',
+    data: { email, password },
+  })
+};
 
-export function getToken(jwt) {
-  return fetch(`${BASE_URL}/users/me`, {
+export function loginUser (email, password) {
+  return request({
+    url: '/signin',
+    data: { email, password },
+  })
+};
+
+export function getToken (token) {
+  return request({
+    url: '/users/me',
     method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${jwt}`,
-    },
-  }).then(checkRes);
-}
-
-function checkRes(res) {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(res.status);
+    token,
+  })
 }
