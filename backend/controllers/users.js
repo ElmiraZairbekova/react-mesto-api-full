3922e96000f9227e9c746fa21d5947238c990179
ElmiraZairbekova <loveni-shop@yandex.ru>
2022-11-26 07:27:39 +0300

    PR15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -41,7 +41,12 @@ const createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(201).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         throw new BadRequest('Переданы некорректные данные при создании пользователя');
